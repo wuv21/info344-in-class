03a41fc8994416d90eaae935b83d41b6d19ea74d
Dave Stearns <dlsinfo@uw.edu>
2017-04-12 09:46:09 -0700

    starter code for hmac demo

--- hmac/main.go
+++ hmac/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+const usage = `
+usage:
+	hmac sign|verify <key> <value>
+`
+
 func main() {
+	if len(os.Args) < 4 ||
+		(os.Args[1] != "sign" && os.Args[1] != "verify") {
+		fmt.Println(usage)
+		os.Exit(1)
+	}
 
 }
