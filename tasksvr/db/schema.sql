64da5f153427bf4b86966796ccddc04dd4062a03
Dave Stearns <dlsinfo@uw.edu>
2017-04-18 15:28:04 -0700

    fix bad spaces/tabs in schema.sql

--- tasksvr/db/schema.sql
+++ tasksvr/db/schema.sql
@@ -1,12 +1,12 @@
 create table tasks (
 	ID serial primary key,
-    title varchar(255),
-    createdAt timestamp,
-    modifiedAt timestamp,
-    complete bool
+	title varchar(255),
+	createdAt timestamp,
+	modifiedAt timestamp,
+	complete bool
 );
 
 create table tags (
 	taskID integer references tasks(ID),
-    tag varchar(64)
+	tag varchar(64)
 );
