42b72c797b7de56931f3f7bfb106c6d130d2c47c
Dave Stearns <dlsinfo@uw.edu>
2017-05-02 12:46:07 -0700

    stub for channels demo

---
+++ channels/main.go
@@ -0,0 +1,17 @@
+package main
+
+//someLongFunc is a function that might
+//take a while to complete, so we want
+//to run it on its own go routine
+func someLongFunc() {
+
+}
+
+func main() {
+	//TODO:
+	//create a channel and call
+	//someLongFunc() on a go routine
+	//passing the channel so that
+	//someLongFunc() can communicate
+	//its results
+}
