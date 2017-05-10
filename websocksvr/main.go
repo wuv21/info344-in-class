d34b43f60bb9ca4e47d0b6e134671fe86a700d37
Dave Stearns <dlsinfo@uw.edu>
2017-05-10 12:55:23 -0700

    add handler context to starter code

--- websocksvr/main.go
+++ websocksvr/main.go
@@ -7,6 +7,12 @@ import (
 	"time"
 )
 
+//HandlerContext provides the HTTP handlers with
+//shared values and interfaces
+type HandlerContext struct {
+	Notifier *Notifier
+}
+
 //MessageEvent represents an event with a message
 //and a timestamp
 type MessageEvent struct {
@@ -18,7 +24,7 @@ type MessageEvent struct {
 //way to create new events for demo purposes. In a real app, you
 //would create and broacast events in response to various handler
 //actions, e.g., new user sign-up, post of a new message, etc.
-func TriggerEvent(w http.ResponseWriter, r *http.Request) {
+func (ctx *HandlerContext) TriggerEvent(w http.ResponseWriter, r *http.Request) {
 	//CORS headers to allow cross-origin requests
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Access-Control-Request-Method", "POST")
@@ -31,7 +37,7 @@ func TriggerEvent(w http.ResponseWriter, r *http.Request) {
 }
 
 //WebSocketUpgradeHandler handles websocket upgrade requests
-func WebSocketUpgradeHandler(w http.ResponseWriter, r *http.Request) {
+func (ctx *HandlerContext) WebSocketUpgradeHandler(w http.ResponseWriter, r *http.Request) {
 	//TODO: upgrade this request to a web socket connection
 	//see https://godoc.org/github.com/gorilla/websocket#hdr-Overview
 	//NOTE that by default, the websocket package will reject
@@ -40,23 +46,23 @@ func WebSocketUpgradeHandler(w http.ResponseWriter, r *http.Request) {
 	//any origin.
 	//See https://godoc.org/github.com/gorilla/websocket#hdr-Origin_Considerations
 
-	//after upgrading, use the `.AddClient()` to add the new
-	//connection to your notifier
+	//after upgrading, use the `.AddClient()` method on your notifier
+	//to add the new client to your notifier's map of clients
 
 }
 
 func main() {
 	addr := "localhost:4000"
 
-	//TODO: create a NewNotifier and call
+	ctx := &HandlerContext{
+		Notifier: NewNotifier(),
+	}
+
+	//TODO: start the notifier by calling
 	//its .Start() method on a new goroutine
-	// go mynotifier.Start()
 
-	//your handlers will need access to this notifer
-	//instance, so share it with them somehow
-
-	http.HandleFunc("/v1/ws", WebSocketUpgradeHandler)
-	http.HandleFunc("/v1/trigger", TriggerEvent)
+	http.HandleFunc("/v1/ws", ctx.WebSocketUpgradeHandler)
+	http.HandleFunc("/v1/trigger", ctx.TriggerEvent)
 
 	fmt.Printf("listening at %s...\n", addr)
 	fmt.Printf("test the server by opening the websockclient/index.html page\n")
