7598ffd5836c13a1d9b9eda0d616c869d21340d0
Dave Stearns <dlsinfo@uw.edu>
2017-05-14 13:23:47 -0700

    starter files for node-mongo demo

---
+++ nodemongo/handlers/tasks.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const express = require('express');
+
+//export a function from this module 
+//that accepts a mongodb Database object
+//and the collection name to use
+module.exports = function(db, colName) {
+
+};
